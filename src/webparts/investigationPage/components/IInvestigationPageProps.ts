290317b931ff11f1e94def5741ba8716ff472c8c
BalajiSenthil-76 <balajis.ece2020@citchennai.net>
2025-09-17 10:37:12 +0530

    first commit

---
+++ src/webparts/investigationPage/components/IInvestigationPageProps.ts
@@ -0,0 +1,7 @@
+export interface IInvestigationPageProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
